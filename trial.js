f734a245af267f285625b42fc00d39d7ac6136eb
Faith-Ndungu <wanjirufaithndunguu#@gmail.com>
2024-04-08 21:23:57 +0300

    added

---
+++ trial.js
@@ -0,0 +1,29 @@
+function arrNumb(numbers){
+
+    let odd = [];
+    let even = [];
+
+    for(let number of numbers){
+        if(number % 2 === 0){
+            even.push(number);
+        } else{
+            odd.push(number)
+        }
+    }
+
+    odd.sort((a,b) => a - b);
+
+    even.sort((a,b) => a-b);
+    console.log(odd)
+    console.log(even)
+
+}
+
+let arr = [1,2,3,4,5,6]
+arrNumb(arr);
+
+
+
+
+
+
